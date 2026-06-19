13ede4771b1b5e73db2a3aa120df951ab4aede57
gestaosistemasaasa <gestao.sistema.saas@gmail.com>
2026-06-19 20:34:16 -0300

    dasd

--- aula1.js
+++ aula1.js
@@ -1,12 +1,12 @@
 const numero1 = 1;
-let numero2 = 2;
+
 
 if (numero1 === 1) {
-    numero2 = 3;
+    numero3 = 3;
 
 }
 
-console.log('resultado', numero2
+console.log('resultado', numero3
 
 )
 
