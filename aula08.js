1df716f02592f03347156b5587c3fe1a024b36ea
Edenir Junior <gestao.sistema.saas@gmail.com>
2026-06-23 10:55:49 -0300

    aula08

---
+++ aula08.js
@@ -0,0 +1,12 @@
+
+function soma(item1, item2){
+    return item1 + item2;
+
+}
+
+function diminuir(item1, item2){
+    return item1 - item2;
+}
+
+console.log('soma', soma(5,6));
+console.log('subtração', diminuir(4242,1244));
