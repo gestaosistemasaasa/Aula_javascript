78f1b4f640b3dc04a98aee1210153e8593e84dad
Edenir Junior <gestao.sistema.saas@gmail.com>
2026-06-23 10:51:12 -0300

    feat: aula07

---
+++ aula7.js
@@ -0,0 +1,6 @@
+const salario = "10355454" ;
+const salarioNumero = Number(salario);
+
+console.log('Salario', salario);
+console.log('salario numero: ', salarioNumero);
+console.log('salario numero:', salarioNumero.toFixed(2));
