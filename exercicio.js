93ad6fe6faf5ca9a7cb33d6670cca89cb9fd3ea3
Edenir Junior <gestao.sistema.saas@gmail.com>
2026-06-23 10:23:13 -0300

    feat Exercicio1

---
+++ exercicio.js
@@ -0,0 +1,5 @@
+const motoristas = ["Carlos", "João", "Pedro", "Ana", "Marcos"];
+
+for (let i = 0; i < motoristas.length; i++){
+    console.log('Motorista  ' + (i+1) +':' , motoristas[i]);
+}
---
+++ exercicio2.js
@@ -0,0 +1,10 @@
+const corridas = [12, 5, 30, 8, 45, 3, 22];
+
+for(let i = 0; i < corridas.length; i++){
+    if(corridas[i]  > 10 )
+    console.log('',corridas[i]);
+}
+
+// Use um for para imprimir SOMENTE as corridas
+// com valor maior que 10
+// Dica: você vai precisar de um if dentro do for
