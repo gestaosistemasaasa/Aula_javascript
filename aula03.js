9bd62b52737cecb29474be88c06cbc3d2c9329fd
Edenir Junior <gestao.sistema.saas@gmail.com>
2026-06-22 21:21:27 -0300

    feat: aula03

---
+++ aula03.js
@@ -0,0 +1,12 @@
+const idade = 28;
+const nome = 'Julio';
+let podeEntrar = false;
+
+if(idade >= 18 && nome ===  'Julio' ) {
+    podeEntrar = true;
+    console.log('Entrou')
+} else{
+    console.log('Não entra!')
+}
+
+
