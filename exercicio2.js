c5e1e9ecfdbcac5f2665e8c273801809341992f6
Edenir Junior <gestao.sistema.saas@gmail.com>
2026-06-24 13:47:16 -0300

    feat: Aulas

--- exercicio2.js
+++ exercicio2.js
@@ -1,10 +1,6 @@
-const corridas = [12, 5, 30, 8, 45, 3, 22];
+let a = 10;
+let b = 9;
+let x = a + b;
 
-for(let i = 0; i < corridas.length; i++){
-    if(corridas[i]  > 10 )
-    console.log('',corridas[i]);
-}
 
-// Use um for para imprimir SOMENTE as corridas
-// com valor maior que 10
-// Dica: você vai precisar de um if dentro do for
+console.log('X =', x);
---
+++ raio.js
@@ -0,0 +1,7 @@
+let a = parseFloat(lines[0]);
+let b = parseFloat(lines[1]);
+let c = parseFloat(lines[2]);
+
+let  media = (a * 2 + b * 3 c * 5) / 10 ;
+
+console.log('MEDIA = ' + media.toFixed(1));
